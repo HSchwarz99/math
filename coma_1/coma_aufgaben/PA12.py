054cb4b2384d5c610ec281ac5fb97be4a31a709d
Hans Schwarzkopf <hans.jonathan.schwarzkopf@ha-es.de>
2020-02-03 10:56:46 +0100

    PA12 finished

--- coma_1/coma_aufgaben/PA12.py
+++ coma_1/coma_aufgaben/PA12.py
@@ -1,5 +1,16 @@
 def maxunimod(L):
-  Laengen = []
+  m = 1
   i = 1
   while i < len(L):
-
+    a = 1
+    while i < len(L) and L[i - 1] <= L[i]:
+      a += 1
+      i += 1
+    while i < len(L) and L[i - 1] >= L[i]:
+      a += 1
+      i += 1
+    if m < a:
+      m = a
+    while i != len(L) and i >= 0 and L[i - 1] == L[i - 2]:
+      i -= 1
+  return m
